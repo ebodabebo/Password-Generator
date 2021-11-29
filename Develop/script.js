925e4868279c06f02363b76725e8f343db92cebc
ebodabebo <hartfield.ebony@gmail.com>
2021-11-28 23:18:57 -0600

    too many errors java, scrapping it all

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,10 @@
 // Assignment code here
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
-}
 
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
 
-function getRandom() {
-  return String.fromCharCode(Math.florr(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  const symbols = '!?@#$%^&*(){}[]=<>/|\:~`,.';
-}
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
