98e53efbdfb906aeedb08f40be061dec09c123df
ebodabebo <hartfield.ebony@gmail.com>
2021-11-28 22:49:45 -0600

    updating Java

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment code here
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
 
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
 
+function getRandom() {
+  return String.fromCharCode(Math.florr(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = '!?@#$%^&*(){}[]=<>/|\:~`,.';
+}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
