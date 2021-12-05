373178f4d8ce270d7e8b5f42005fd8085b7de6b1
ebodabebo <hartfield.ebony@gmail.com>
2021-12-05 14:01:55 -0600

    saving progress on password generator

--- Develop/script.js
+++ Develop/script.js
@@ -6,11 +6,44 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var length = prompt("How long would you like your password?")
+  var lowerCase = confirm("Do you want lower case letter?")
+  var upperCase = confirm("Do you want upper case letters?")
+  var numbers = confirm("Do you want numbers?")
+  var specialCharacters = confirm("Do you want special characters?")
+  var passwordOptions="" 
+  
+  if (lowerCase === true){
+    passwordOptions="abcdefghijklmnopqrstuvwxyz"
+  }
+
+  if (upperCase === true){
+    passwordOptions+="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+
+  if (numbers === true){
+    passwordOptions+="0123456789"
+  }
+
+  if (specialCharacters === true){
+    passwordOptions+="!@#$%^&*?~"
+  }
+
+  function stringGen(yourNumber){
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*?~"
+
+  }
+
+  console.log(passwordOptions)
+
+
+
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
 // Add event listener to generate button
